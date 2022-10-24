aa150dddc172171c3f48b02992b8c4db4465e1ec
Alonso Paucar <iaquintanapa@gmail.com>
2022-10-23 21:44:25 -0500

    Unas cosas mas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+
+  nombre: string = 'caluMon digivOluciona';
+  valor: number  = 1000;
+  obj = {
+    nombre: 'Calumon'
+  }
+
+  mostrarNombre() {
+    console.log( this.nombre )
+    console.log( this.valor )
+    console.log( this.obj )
+  }
+
 }
