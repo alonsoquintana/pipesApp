7da6c022b778c31130e81c39de73f2410f586db0
Alonso Paucar <iaquintanapa@gmail.com>
2022-10-25 21:22:14 -0500

    i8nSelectPipe e i18nPluralPipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  nombre: string = 'caluMon digivOluciona';
-  valor: number  = 1000;
-  obj = {
-    nombre: 'Calumon'
-  }
+  constructor(private primengConfig: PrimeNGConfig) {}
 
-  mostrarNombre() {
-    console.log( this.nombre )
-    console.log( this.valor )
-    console.log( this.obj )
+  ngOnInit() {
+    this.primengConfig.ripple = true;
   }
 
 }
--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-no-comunes',
-  templateUrl: './no-comunes.component.html'
+  templateUrl: './no-comunes.component.html',
+  styles: [    
+  ]
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
 
-  constructor() { }
+  // i18nSelect  
+  nombre: string = 'pablo';
+  genero: string = 'masculino';
+  invitacionMapa = {
+    'masculino': 'invitarlo',
+    'femenino' : 'invitarla'
+  }
 
-  ngOnInit(): void {
+  // i18nPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Hernando', 'Eduardo', 'Fernando', 'Pablo'];
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando.',
+    'other':"tenemos # clientes esperando."
   }
 
 }
