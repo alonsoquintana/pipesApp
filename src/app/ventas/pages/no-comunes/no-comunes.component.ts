447dfdfe8eb617cf16b1d8099cc27546a6589abc
Alonso Paucar <iaquintanapa@gmail.com>
2022-10-27 19:49:59 -0500

    Terminando la primera parte

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -33,5 +34,41 @@ export class NoComunesComponent {
   borrarCliente() {
     this.clientes.pop();
   }
+
+
+  // KeyValue Pipe
+  persona = {
+    nombre: 'Calumon',
+    edad: 45,
+    direccion: 'Digimundo'
+  }
+
+  // JsonPipe
+  heroes = [
+    {
+      nombre: 'Superman',
+      vuela: true
+    },
+    {
+      nombre: 'Robin',
+      vuela: false
+    },
+    {
+      nombre: 'Aquaman',
+      vuela: false
+    },
+  ]
+
+
+  // Async Pipe
+  miObservable = interval(1000); // 0,1,2,3,4,5,6,
+
+  valorPromesa = new Promise( (resolve, reject) => {
+
+    setTimeout(() => {
+      resolve( 'Tenemos data de promesa' );
+    }, 3500 );
+
+  });
 
 }
